b364ad3ecc706ecc23ec15cc6ce00934e7d828b2
fnqbruce <bruce.belson@my.jcu.edu.au>
2017-12-21 14:26:07 +1000

    Downloads bibtex files directly
    New infrastructure and workflow

--- README.md
+++ README.md
@@ -1,10 +1,30 @@
-Name: node-web-systematic-review
+# Origin 
+This repo is forked from the following:
 
-Version: 1.0.0
+> Name: node-web-systematic-review
+> Version: 1.0.0
+> Author: Luis Augusto Melo Rohten
+> Date: 05/02/2016
+> Execute: node app.js
 
-Author: Luis Augusto Melo Rohten
+# Purpose
+The main purpose of the script remains the same: to download citations from the SpringerLink web-site.
 
-Date: 05/02/2016
+# Changes
+## Infrastructure
 
+New infrastructure has been added, to help with URL construction, and to make the script more general.
 
-Execute: node app.js
+Logging has been added.
+
+## Workflow
+
+The script downloads the SpringerLink citation page via the URL on the article page, in bibtex format. Formerly, the citiation was composed from screen-scraped fragments on the article page.
+
+The script runs until all requests have returned or failed, and reports the total counts of found, missing and null citations.
+
+All bibtex pages are concatenated into a single output file: springerlink.bib.
+
+# Future changes
+
+It is intended to add support for IEEE and ScienceDirect databases. These will require new infrastructure, particularly for AJAX (asynchronous) result pages.
--- springer/search-springer.js
+++ springer/search-springer.js
@@ -1,167 +1,191 @@
-module.exports = function(){
-      var fs = require('fs');
-      var request = require('request');
-      var cheerio = require('cheerio');
-      var json2csv = require('json2csv');
+// Based on code by Luis Augusto Melo Rohten
+// https://github.com/luisaugustomelo/node-web-systematic-review
 
-      return {
-            execute: function(){
-                  console.log('Application Start... Wait!');
-                  fs.exists('output.csv', function(exists) {
-                      if (exists) {
-                          fs.unlinkSync('output.csv');
-                      }
-                  });
+module.exports = function () {
+  var fs = require('fs');
+  var request = require('request');
+  var cheerio = require('cheerio');
 
-                  //main_page = 'http://link.springer.com/search?query=%28%22Wireless+Sensor%22+OR+%22Sensor+networks%22+OR+%22Actuator+Networks%22+OR+%22Internet+of+Things%22+OR+%22Web+of+Things%22%29+AND+%28%22systematic+literature+review%22+OR+%22systematic+review%22+OR+%22systematic+mapping%22+OR+%22mapping+study%22+OR+%22systematic+literature+mapping%22%29';
-                  main_page = 'http://link.springer.com/search?query=%28%22Wireless+Sensor%22+OR+%22Sensor+networks%22+OR+%22Actuator+Networks%22+OR+%22Internet+of+Things%22+OR+%22Web+of+Things%22%29+AND+%28%28%22Model-Driven%22+OR+%22MDA%22+OR+%22MDE%22+OR+%22MDD%22%29+OR+%28%22BPMN%22+OR+%22BPM%22+OR+%22Business+Process%22%29%29';
-                  var length = 0;
-                  var title, abstract, keywords, authors, year;
-                  var fields = ['title', 'abstract', 'keywords', 'authors', 'year'];
-                  var number_articles = 0;
-                  //var logStream = fs.createWriteStream('output.csv', {'flags': 'a'});
+  /////////////////////////////////////////////////////////////////////
+  // Constants
+  /////////////////////////////////////////////////////////////////////
+  var theLanguage = 'En';
+  var theExpr = '(coroutine OR coroutines OR "lightweight+threads"'
+    + ' OR "co-routine" OR "co-routines")'
+    + ' AND (IoT OR "Internet of Things" OR "Cyber Physical Systems"'
+    + ' OR "Cyber-physical Systems" OR RTOS OR "Real-time Operating Systems"'
+    + ' OR "Embedded Systems" OR WSN OR "Wireless sensor networks" OR WSAN)';
+  var theStartDate = 2007;
+  var theEndDate = 2017;
+  var theOutputFile = 'springerlink.bib';
 
-                  request(main_page, function(error, response, html){ //<-- abre o request(main_page
-                  if(!error){ //<-- abre o if(!error) do request
-                      var $ = cheerio.load(html);
+  /////////////////////////////////////////////////////////////////////
+  // URL builder helpers
+  /////////////////////////////////////////////////////////////////////
 
-                      $('span.number-of-pages').filter(function(){
-                          length = $(this).text();
-                      });
+  // Apply HTML escaping to the various parts of the query URL
+  var esc = function (raw) {
+    return raw
+      .replace(/\"/g, '%22')
+      .replace(/\(/g, '%28')
+      .replace(/\)/g, '%29')
+      .replace(/ /g, '+');
+  };
+  var datePart = function (startYear, endYear) {
+    return "date-facet-mode=between&facet-start-year=" + startYear
+      + "&facet-end-year=" + endYear;
+  };
+  var languagePart = function (lang) {
+    return esc('facet-language="' + lang + '"');
+  };
+  var queryPart = function (expr) {
+    return 'query=' + esc(expr);
+  };
 
-                      $('h1.number-of-search-results-and-search-terms').filter(function(){
-                            number_articles = $(this).children().first().text();
-                            console.log('number-articles:' + number_articles);
-                      });
+  var buildUrl = function (page) {
+    return 'https://link.springer.com/search'
+      + ((page >= 0) ? '/page/' + (page + 1) : '')
+      + '?' + datePart(theStartDate, theEndDate) + '&' + languagePart(theLanguage) + '&' + queryPart(theExpr);
+  };
 
+  /////////////////////////////////////////////////////////////////////
+  // Logger
+  /////////////////////////////////////////////////////////////////////
 
-                    } //<-- fecha o if(!error) do request
+  var log_header = function() {
+    console.log('level,description,page,item,url,data0,data1,data2');
+  }
+  var log_event = function(level, description, page, item, url, data0, data1, data2) {
+    console.log(level + ',' + description + ',' + (page == null ? '' : page) + ',' 
+      + (item == null ? '' : item) + ',' + (url == null ? '' : url) + ',' 
+      + (data0 == null ? '' : data0) + ',' + (data1 == null ? '' : data1) + ',' + (data2 == null ? '' : data2));
+  }
 
-                  var url = "";
-                  /*Site não utilizado, mas pode ser útil:
-                  https://www.digitalocean.com/community/tutorials/how-to-use-node-js-request-and-cheerio-to-set-up-simple-web-scraping
-                  */
-                  (function(){
+  /////////////////////////////////////////////////////////////////////
+  // The procedure
+  /////////////////////////////////////////////////////////////////////
 
-                      articles = [];
-                      count = 1;
-                      var page = 0;
+  return {
+    execute: function () {
+      log_header();
+      log_event(0, "Application start");
+      fs.exists(theOutputFile, function (exists) {
+        if (exists) {
+          log_event(0,'Deleting output file',null,null,theOutputFile);
+          fs.unlinkSync(theOutputFile);
+        }
+      });
 
-                      console.log('Reading pages NOW!');
-                      while(page <= length){ //abre o while(page < length)
-                          //url = 'http://link.springer.com/search/page/' + (page+1) + '?query=%28%22Wireless+Sensor%22+OR+%22Sensor+networks%22+OR+%22Actuator+Networks%22+OR+%22Internet+of+Things%22+OR+%22Web+of+Things%22%29+AND+%28%22systematic+literature+review%22+OR+%22systematic+review%22+OR+%22systematic+mapping%22+OR+%22mapping+study%22+OR+%22systematic+literature+mapping%22%29';
-                          url = 'http://link.springer.com/search/page/' + (page+1) + '?query=%28%22Wireless+Sensor%22+OR+%22Sensor+networks%22+OR+%22Actuator+Networks%22+OR+%22Internet+of+Things%22+OR+%22Web+of+Things%22%29+AND+%28%28%22Model-Driven%22+OR+%22MDA%22+OR+%22MDE%22+OR+%22MDD%22%29+OR+%28%22BPMN%22+OR+%22BPM%22+OR+%22Business+Process%22%29%29';
+      main_page = buildUrl(-1);
+      var num_pages = 0;
+      var num_articles = 0;
+      var num_citations = 0;
+      var num_failures = 0;
+      var num_empty = 0;
+      var results_reported = false;
 
-                          request(url, function(error, response, html){ //abre o request(url
-                          if(!error){//abre o error do request(url
-                              var $ = cheerio.load(html);
-                              $('#results-list li .title').each(function(i, elm) {
-                                  url = 'http://link.springer.com' + $(this).attr('href');
-
-                                  request(url, function(error, response, html){
-                                    //var json
-                                    json = { title : "", abstract : "", keywords : "", authors : "", year : ""};
-                                    if(!error){
-                                        var $ = cheerio.load(html);
-                                        number = i+1;
-                                        //Title
-                                        $('h1.ArticleTitle').filter(function(){
-                                            var data = $(this);
-                                            json.title = data.text();
-                                        })
-
-                                        //Title
-                                        $('h1.ChapterTitle').filter(function(){
-                                            var data = $(this);
-                                            json.title = data.text();
-                                        })
-
-                                        //Abstract
-                                        $('p.Para').filter(function(){
-                                            var data = $(this);
-                                            json.abstract = data.text();
-                                        })
-
-                                        //Keywords
-                                        $('span.Keyword').filter(function(){
-                                            var data = $(this);
-                                            if(json.keywords == "")
-                                              json.keywords = data.text();
-                                            else
-                                              json.keywords = json.keywords + " - " + data.text();
-                                        })
-
-                                        //Authors
-                                        $('span.AuthorName').filter(function(){
-                                            var data = $(this);
-                                            if(json.authors == "")
-                                              json.authors = data.text();
-                                            else
-                                              json.authors = json.authors + " ; " + data.text();
-                                        })
-
-                                        //Year
-                                        $('div#abstract-actions.panel').filter(function(){
-                                            var a = $(this);
-                                            json.year = $('input[name="copyrightyear"]').prop('value');
-                                        })
-
-                                        /*if(((JSON.stringify(json, 4, 4)).toLowerCase().indexOf("wireless sensor") > -1 ||
-                                      (JSON.stringify(json, 4, 4)).toLowerCase().indexOf("wireless networks") > -1||
-                                    (JSON.stringify(json, 4, 4)).toLowerCase().indexOf("actuator networks") > -1 ||
-                                  (JSON.stringify(json, 4, 4)).toLowerCase().indexOf("internet of things") > -1 ||
-                                (JSON.stringify(json, 4, 4)).toLowerCase().indexOf("web of things") > -1) &&
-                                  ((JSON.stringify(json, 4, 4)).toLowerCase().indexOf("systematic literature review") > -1 ||
-                                    (JSON.stringify(json, 4, 4)).toLowerCase().indexOf("systematic review") > -1 ||
-                                     (JSON.stringify(json, 4, 4)).toLowerCase().indexOf("systematic mapping") > -1 ||
-                                       (JSON.stringify(json, 4, 4)).toLowerCase().indexOf("mapping study") > -1 ||
-                                         (JSON.stringify(json, 4, 4)).toLowerCase().indexOf("systematic literature mapping") > -1))*/
-
-
-                                         if(((JSON.stringify(json, 4, 4)).toLowerCase().indexOf("wireless sensor") > -1 ||
-                                      (JSON.stringify(json, 4, 4)).toLowerCase().indexOf("sensor networks") > -1||
-                                    (JSON.stringify(json, 4, 4)).toLowerCase().indexOf("actuator networks") > -1 ||
-                                  (JSON.stringify(json, 4, 4)).toLowerCase().indexOf("internet of things") > -1 ||
-                                (JSON.stringify(json, 4, 4)).toLowerCase().indexOf("web of things") > -1) &&
-                                  ((JSON.stringify(json, 4, 4)).toLowerCase().indexOf("model-driven") > -1 ||
-                                    (JSON.stringify(json, 4, 4)).toLowerCase().indexOf("mda") > -1 ||
-                                     (JSON.stringify(json, 4, 4)).toLowerCase().indexOf("mde") > -1 ||
-                                       (JSON.stringify(json, 4, 4)).toLowerCase().indexOf("mdd") > -1 ||
-                                          (JSON.stringify(json, 4, 4)).toLowerCase().indexOf("bpmn") > -1 ||
-                                                (JSON.stringify(json, 4, 4)).toLowerCase().indexOf("bpm") > -1 ||
-                                                      (JSON.stringify(json, 4, 4)).toLowerCase().indexOf("business process") > -1)) {
-                                                            //console.log(JSON.stringify(json,4,4));
-                                                            //fs.appendFile('output.txt', JSON.stringify(json,4,4));
-                                                            articles.push(json);
-                                         }
-
-                                         //console.log(count);
-                                         if(count == number_articles){
-                                               console.log('Writing articles in file.');
-                                               json2csv({ data: articles, fields: fields }, function(err, csv) {
-                                                 if (err) console.log(err);
-                                                 fs.writeFile('output.csv', csv, function(err) {
-                                                   if (err) throw err;
-                                                   console.log('File Saved!\nApplication finished, thanks!');
-
-                                                 });
-                                               });
-                                         }
-
-
-                                         count = count + 1;
-                                    }
-                                    else {
-                                        console.log('Erro: '+error);
-                                    }
-                                  })
-                              })
-                            }//fecha o error do request(url
-                          })//fecha o request(url
-                          page++;
-                        } //fecha o while(page < length)
-                    }())
-                  }) //<-- fecha o request(main_page
-            }
+      var finishIfReady = function () {
+        if (!results_reported && (num_citations + num_failures + num_empty == num_articles)) {
+          results_reported = true;
+          log_event(0, 'Complete - writing articles to file.', null, null, theOutputFile, num_citations, num_empty, num_failures);
+          fs.writeFile(theOutputFile, bibs, function (err) {
+            if (err) throw err;
+            log_event(0, 'Closing');
+          });
+        }
       };
-}
+
+      log_event(0, 'Loading main page', null, null, main_page);
+      request(main_page, function (error, response, html) { 
+        log_event(0, 'Received main page', null, null, main_page);
+        if (!error) { //<-- abre o if(!error) do request
+          var $ = cheerio.load(html);
+
+          $('span.number-of-pages').first().filter(function () {
+            num_pages = $(this).text();
+            log_event(0, 'num_pages', null, null, main_page, num_pages);
+          });
+
+          $('h1.number-of-search-results-and-search-terms').first().filter(function () {
+            num_articles = $(this).children().first().text();
+            log_event(0, 'num_articles', null, null, main_page, num_articles);
+          });
+        }
+        else {
+          log_event(0, 'Main page failed', null, null, main_page, error);
+        }
+
+        var url = '';
+        /* Useful information here...
+          https://www.digitalocean.com/community/tutorials/how-to-use-node-js-request-and-cheerio-to-set-up-simple-web-scraping
+        */
+        (function () {
+
+          bibs = '';
+          if (num_pages > 0) {
+            log_event(0, 'About to read index pages', null, null, null, "num_pages=" + num_pages);
+          }
+          for (var page = 0; page < num_pages; page++) {
+            (function (page) {  // closure to preserve page number within request callback
+              url = buildUrl(page);
+              log_event(1, 'Loading index page', page, null, url);
+              request(url, function (error, response, html) { //abre o request(url
+                log_event(1, 'Received index page', page, null, url);
+                if (error) {
+                  log_event(1, 'Fatal error - could not read index page', page, null, url, error);
+                }
+                else { // We have a search page, containing a list of items, each of which we need to inspect  
+                  var $ = cheerio.load(html);
+                  var elements = $('#results-list li .title');
+                  log_event(1, 'Element count', page, null, url, elements.length);
+                  elements.each(function (i, elm) { // another closure, this time to isolate i, the index within the page.
+                    url = 'https://link.springer.com' + $(this).attr('href');
+                    log_event(2, 'Loading result page', page, i, url);
+                    request(url, function (error, response, html) {
+                      log_event(2, 'Received result page', page, i, url);
+                      if (!error) {
+                        var $ = cheerio.load(html);
+                        var cite_links = $('a.gtm-export-citation[data-gtmlabel="BIB"]').first();
+                        if (cite_links.length > 0) {
+                          cite_links.filter(function () {
+                            var data = $(this);
+                            url = 'https:' + data.attr('href');
+                            log_event(3, 'Loading citation info', page, i, url);
+                            request(url, function (error, response, text) {
+                              log_event(3, 'Received citation info', page, i, url);
+                              if (!error) {
+                                num_citations++;
+                                bibs += text; // text.replace('\n', '\r\n');
+                                log_event(4, 'Saved citation', page, i, url, num_citations, num_empty, num_failures);
+                              }
+                              else {
+                                num_failures++;
+                                log_event(4, 'Error loading citation', page, i, url, num_citations, num_empty, num_failures);
+                              }
+                              finishIfReady();
+                            });
+                          });
+                        }
+                        else {
+                          num_empty++;
+                          log_event(3, 'No citation link', page, i, url, num_citations, num_empty, num_failures);
+                          finishIfReady();
+                        }
+                      }
+                      else {
+                        log_event(2, 'Error on result page', page, i, url, error);
+                        num_failures++;
+                        finishIfReady();
+                      }
+                    })
+                  })
+                } // Inspect a serach result page
+              }) // End of request for a search result page (perhaps with error)
+
+            })(page); // Closure for page
+          }
+        }());
+      }) // Request the main page: end of call-back
+    } // end of execute()
+  }; // end of Module return value
+} // end of Module
